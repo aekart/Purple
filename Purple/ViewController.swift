de5e8c1b27508100305a121e8d3fee230e1eb823
Grischa Ekart <gekart@GrischasMacBook.fritz.box>
2015-12-12 09:58:02 +0100

    Einige Änderungen gemacht

--- Purple/ViewController.swift
+++ Purple/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBAction func zeigeName() {
+        nameLabel.text = "Hello Annika"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
